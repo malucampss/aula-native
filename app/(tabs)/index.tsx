57cba8ddfd92a38cda31597deafe20333cf08bc8
Maria Luisa <camposmarialuisa645@gmail.com>
2025-08-01 14:17:57 -0300

    aula react native

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,52 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import React, { useState } from 'react';
+import { StyleSheet, Image, TextInput, Button, } from 'react-native';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [email, setEmail] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Aba Um</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      
+      <Image source={require('@/assets/images/images.png')} style={styles.image} />
+
+      /* Formulário */
+      <TextInput
+        placeholder="Nome"
+        value={nome}
+        onChangeText={setNome}
+      />
+      <TextInput
+        placeholder="Sobrenome"
+        value={sobrenome}
+        onChangeText={setSobrenome}
+      />
+      <TextInput
+        placeholder="CPF"
+        inputMode="numeric"
+        value={cpf}
+        onChangeText={setCpf}
+      />
+      <TextInput
+        placeholder="Telefone"
+        inputMode="numeric"
+        value={telefone}
+        onChangeText={setTelefone}
+      />
+      <TextInput
+        placeholder="E-mail"
+        inputMode="email"
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <Button title="ENVIAR"  onPress={() => { alert(nome + ' ' + sobrenome + ' ' + cpf + ' ' + telefone + ' ' + email) }} />
     </View>
   );
 }
@@ -18,6 +56,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
   title: {
     fontSize: 20,
@@ -27,5 +66,19 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
   },
 });
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,51 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
+  const [texto, atualizaTexto] = useState('Olá Mundo!');
+  const [email, atualizaEmail] = useState('exemplo@mail.com');
+  const [telefone, atualizaTelefone] = useState('(11) 3325-5577');
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Aba Dois</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Image
+      style= {{
+        width: 50,
+        height: 50
+      }}
+      source={{
+        uri: 'https://reactnative.dev/img/tiny_logo.png?authuser=0'
+      }}
+      />
+      <TextInput  
+      value={texto}
+      onChangeText={atualizaTexto}
+      />
+      <TextInput 
+      style={{
+        fontSize:50
+      }}
+      value={telefone}
+      onChangeText={atualizaTelefone}
+      inputMode='numeric'
+      />
+      <TextInput 
+      style= {{
+        fontSize: 30
+      }}
+      value={email}
+      onChangeText={atualizaEmail}
+      inputMode='email'
+      />
+
+      <Button 
+      title='ENVIAR'
+      onPress={() => { alert(texto + '' + telefone) }}
+      />
     </View>
   );
 }
